1d373fe6245cdb1cfdb739e3e83f29c64f22e7be
Zhilei Zhou <zh676054@dal.ca>
2018-03-03 21:07:23 -0400

    03-03

---
+++ Pyfiles/before 2-28/open_reading_frame.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 19:52:16 2018
+
+@author: zhouzhilei
+"""
+
+#!usr/bin/env python3
+
+def find_open_reading_frame(seq):
+    import Translating_RNA_into_Protein
+    
+    
